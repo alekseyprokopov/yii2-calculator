d30eca77daba6defae26d435d5b5d5d1bf0e9ed9
Тишкин Артем Владимирович <cr_ovr_web4@efko.ru>
2023-07-05 16:59:44 +0300

    fix problems

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -18,15 +18,15 @@ class ApiController extends ActiveController
 
         $repository = new PricesRepository(Yii::$app->params['prices']);
         $model = new CalculatorForm();
-        
+
         $model->load(Yii::$app->request->get(), '');
         $price = $repository->getResultPrice($model->type, $model->tonnage, $model->month);
 
-        if (isset($price) == false) {
-            return Yii::$app->response->data = ["error" => 404];
+        if (isset($price) === false) {
+            return ["error" => 404];
         }
 
         $priceList = $repository->getRawPricesByType($model->type);
-        return Yii::$app->response->data = ['price' => $price, $model->type => $priceList];
+        return ['price' => $price, $model->type => $priceList];
     }
 }
